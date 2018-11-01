222af6195d0cdcf16f03588beffb1ab0c756b28e
Fántics Attila <fantics.attila@windowslive.com>
2018-11-01 17:40:39 +0100

    Első Razor Page-em! :)
    /Booklist/index

---
+++ Booklist_Razor_Pages/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Booklist_Razor_Pages.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        public string someData { get; set; }
+
+        public void OnGet()
+        {
+            someData = "This is first property!";
+
+        }
+    }
+}
